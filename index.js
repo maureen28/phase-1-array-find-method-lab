c6b50b8f9cee2158b656fd21a10cf6743a7e244d
maureen <maureen.murimi@moringaschool.com>
2022-05-14 12:20:25 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// code your solution here
+let superbowlWin = superbowlWinRecord => {
+	let superWon = superbowlWinRecord.find( superbowl => {  return superbowl.result === "W" })
+	if (superWon){ 
+		return superWon.year
+	} else {
+		return undefined
+	}
+}
